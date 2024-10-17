696838274cd13421568847c9f4e202da46ba834b
Alessandro Becci <trofui@gmail.com>
2024-10-17 12:09:42 +0200

    Starting to implement plugin for Assignment #1 of SAP

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.jar {
+    archiveFileName.set("app.jar")
+}
--- src/main/java/core/application/ColorStatePlugin.java
+++
@@ -1,4 +0,0 @@
-package core.application;
-
-public interface ColorStatePlugin {
-}
