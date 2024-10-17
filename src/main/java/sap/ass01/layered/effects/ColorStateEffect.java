425388b8f80f84f17a4de8b286e25c33511ac2c5
Alessandro Becci <trofui@gmail.com>
2024-10-17 16:53:25 +0200

    updated plugin

--- src/main/java/sap/ass01/layered/effects/ColorStateEffect.java
+++ src/main/java/sap/ass01/layered/effects/ColorStateEffect.java
@@ -1,6 +1,5 @@
 package sap.ass01.layered.effects;
 
-
 import sap.ass01.layered.plugin.ColorStatePlugin;
 import sap.ass01.layered.ui.models.EBikeViewModel;
 
@@ -10,6 +9,12 @@ public class ColorStateEffect implements ColorStatePlugin {
 
     @Override
     public EBikeViewModel colorState(EBikeViewModel bike) {
-        return bike.updateColor(Color.GREEN);
+        String state = bike.state().toUpperCase();
+        return switch (state) {
+            case "AVAILABLE" -> bike.updateColor(Color.GREEN);
+            case "IN_USE" -> bike.updateColor(Color.YELLOW);
+            case "MAINTENANCE" -> bike.updateColor(Color.RED);
+            default -> bike;
+        };
     }
 }
